16ebfac81585b2d433cb21325f08d294bd9e3203
Dipu Ratheesan <dipu.ratheesan@imaginea.com>
2017-12-03 12:29:47 +0530

    Building React Applications with Idiomatic Redux     Finished 5th video

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,14 +1,22 @@
 import React from 'react';
-import {Provider} from 'react-redux';
+import PropTypes from 'prop-types';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import TodoApp from '../containers/TodoApp';
 
 
 const Root = (props) => {
     return (
         <Provider store={props.store}> 
-            <TodoApp />
+            <Router>
+                <Route path='/' component={TodoApp} />
+            </Router>
         </Provider>
     );
+}
+
+Root.propTypes = {
+    store: PropTypes.object.isRequired
 }
 
 export default Root;
