60f6dd86bb298183b847a9c40c4b23aa5e988f4d
Dipu Ratheesan <dipu.ratheesan@imaginea.com>
2017-12-10 18:23:16 +0530

    Building React Applications with Idiomatic Redux, finished 17th video,  Applying Redux Middleware

--- src/lib/configureStore.js
+++ src/lib/configureStore.js
@@ -1,47 +1,19 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';   
+import logger from 'redux-logger';
+import promise from 'redux-promise';
 import throttle from 'lodash/throttle';
 import todoappreducer from '../reducers/';
 
-const logger = (store) => {
-    return (next) => {
-        if(!console.group) {
-            return next;
-        }
-        return (action) => {
-            console.group(action.type);
-            console.log('%c Prev state', 'color:gray', store.getState());
-            console.log('%c Action', 'color:blue', action);
-            const returnValue = next(action);
-            console.log('%c Next state', 'color:green', store.getState());
-            console.groupEnd(action.type);
-            return returnValue;
-        }
-    }
-}
-
-const promise = (store) => {
-    return (next) => {
-        return (action) => {
-        if (typeof action.then === 'function') {
-            return action.then(next);
-        }
-        return next(action);
-        };
-    }
-};
-
-const wrapDispatchWithMiddlewares = (store, middlewares) => {
-    middlewares.slice().reverse().forEach(middleware =>
-        store.dispatch = middleware(store)(store.dispatch)
-    );
-}
 const configureStore = () => {
-    const store = createStore(todoappreducer);
     const middlewares = [promise];
     middlewares.push(logger);
-    wrapDispatchWithMiddlewares(store, middlewares);
-    return store;
-};
+    
+  
+    return createStore(
+      todoappreducer,
+      applyMiddleware(...middlewares)
+    );
+  };
 
 export default configureStore;
 
