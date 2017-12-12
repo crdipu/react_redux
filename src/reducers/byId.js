908f659bc3cc012e81afdadbe2bdd91e226347b9
Dipu Ratheesan <dipu.ratheesan@imaginea.com>
2017-12-12 21:35:18 +0530

    Added delay of one more second in api.js Formatted code in byId.js

--- src/reducers/byId.js
+++ src/reducers/byId.js
@@ -1,20 +1,24 @@
 import * as types from '../constants/types';
 
 const byId = (state = {}, action) => {
-    switch (action.type) {
-      case types.RECEIVE_TODOS: // eslint-disable-line no-case-declarations
-        const nextState = { ...state };
-        action.response.forEach(todo => {
+  switch (action.type) {
+    case types.RECEIVE_TODOS: // eslint-disable-line no-case-declarations
+      const nextState = {
+        ...state
+      };
+      action
+        .response
+        .forEach(todo => {
           nextState[todo.id] = todo;
         });
-        return nextState;
-      default:
-        return state;
-    }
+      return nextState;
+    default:
+      return state;
+  }
 };
 
 export default byId;
 
 export const getTodo = (state, id) => {
-    return state[id];
+  return state[id];
 }
